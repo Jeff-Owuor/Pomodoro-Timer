d8f09896b3948d7c8375a5fc97ed765b7d323d7f
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-01-21 17:44:02 +0300

    Render the timer component to the page
    import the timer class from timer file

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
+import Timer from './Timer';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component{      
+      render(){
+           return (
+            <Timer/>
+           )
+        } 
+      
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    
+  },
+  containerElement : {
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+  count:{
+    fontSize: 48,
+  }
 });
